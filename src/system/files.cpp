b9cc46358ab47cea0be08fc2f8fab66904b049ae
Fabio Arnold <fabio@fabioarnold.de>
2016-08-03 19:07:18 +0200

    fixed nasty windows bug where loaded strings contained #linecount garbage bytes

--- src/system/files.cpp
+++ src/system/files.cpp
@@ -26,20 +26,21 @@ u8 *readDataFromFile(const char *filepath, size_t *out_data_size) {
 }
 
 char *readStringFromFile(const char *filepath, int *str_len) {
-	FILE *file = fopen(filepath, "r");
+	FILE *file = fopen(filepath, "r"); // r means textmode: drops \r on windows so file_size != str_len
 	if (!file) {
 		LOGE("Couldn't read %s", filepath);
 		return NULL;
 	}
 
 	size_t file_size = getFileSize(file);
-	char *str = new char[file_size + 1];
-	fread(str, sizeof(char), file_size, file);
-	str[file_size] = '\0'; // zero terminated string
+	char *str = new char[file_size + 1]; // we need at least this much storage
+	size_t bytes_read = fread(str, sizeof(char), file_size, file);
+	assert(bytes_read <= file_size);
+	str[bytes_read] = '\0'; // zero terminated string
 	fclose(file);
 
 	if (str_len) {
-		*str_len = (int)file_size-1;
+		*str_len = (int)bytes_read;
 	}
 
 	return str;
