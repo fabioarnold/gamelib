4c5be6cfc15261d4d8c4d216733f0f92c2c523c2
Fabio Arnold <fabio@fabioarnold.de>
2017-04-08 14:52:49 +0200

    changes to camera api

--- src/video/camera.h
+++ src/video/camera.h
@@ -5,11 +5,20 @@ struct Camera {
 
 	vec3 location;
 	vec3 euler_angles;
+
+	mat3 rot_mat;
 	mat4 view_mat;
 
-	void updateProjectionMatrix();
-	void updateViewMatrix();
+	mat4 view_proj_mat;
+
+	void setPerspectiveProjection(float near_plane, float far_plane);
+	void setOrthographicProjection(VideoMode video, float near_plane, float far_plane);
+	void updateRotationMatrix(); // euler to rot_mat
+	void updateViewMatrix() { view_mat = m4(rot_mat) * translationMatrix(-location); }
+	void updateViewProjectionMatrix() { view_proj_mat = proj_mat * view_mat; }
 
 	vec3 makeViewRay(int screen_x, int screen_y, VideoMode video);
-	mat4 makeInverseViewMatrix();
+
+	mat3 getInverseRotationMatrix() { return transpose(rot_mat); }
+	mat4 getInverseViewMatrix() { return translationMatrix(location) * m4(getInverseRotationMatrix()); }
 };
