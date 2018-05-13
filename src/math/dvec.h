e906f0600fb4ee945c72d36fc586252814b980a5
Fabio Arnold <fabio@fabioarnold.de>
2018-05-13 23:16:21 +0200

    add dvec.h

---
+++ src/math/dvec.h
@@ -0,0 +1,82 @@
+union dvec3 {
+	struct {double x, y, z;};
+	struct {double r, g, b;};
+	double e[3];
+
+	inline double& operator[](int index) {
+		assert(index >= 0 && index < 3);
+		return e[index];
+	}
+};
+
+inline dvec3 dv3(double f) {
+	dvec3 v; v.x = f; v.y = f; v.z = f;
+	return v;
+}
+inline dvec3 dv3(double x, double y, double z) {
+	dvec3 v; v.x = x; v.y = y; v.z = z;
+	return v;
+}
+/* compatibility with vec.h */
+inline vec3 v3(dvec3 dv) {
+	vec3 v;
+	v.x = (float)dv.x;
+	v.y = (float)dv.y;
+	v.z = (float)dv.z;
+	return v;
+}
+
+inline dvec3 operator+(dvec3 v0, dvec3 v1) {
+	dvec3 result;
+	result.x = v0.x + v1.x;
+	result.y = v0.y + v1.y;
+	result.z = v0.z + v1.z;
+	return result;
+}
+inline dvec3 operator-(dvec3 v0, dvec3 v1) {
+	dvec3 result;
+	result.x = v0.x - v1.x;
+	result.y = v0.y - v1.y;
+	result.z = v0.z - v1.z;
+	return result;
+}
+inline dvec3 operator*(double f, dvec3 v) {
+	dvec3 result;
+	result.x = f * v.x;
+	result.y = f * v.y;
+	result.z = f * v.z;
+	return result;
+}
+inline dvec3 operator/(dvec3 v, double d) {
+	dvec3 result;
+	result.x = v.x / d;
+	result.y = v.y / d;
+	result.z = v.z / d;
+	return result;
+}
+
+inline float dot(dvec3 v0, dvec3 v1) {
+	return v0.x*v1.x + v0.y*v1.y + v0.z*v1.z;
+}
+
+inline dvec3 cross(dvec3 v0, dvec3 v1) {
+	dvec3 result;
+	result.x = v0.y*v1.z - v0.z*v1.y;
+	result.y = v0.z*v1.x - v0.x*v1.z;
+	result.z = v0.x*v1.y - v0.y*v1.x;
+	return result;
+}
+
+inline float lengthSqr(dvec3 v) {
+	return dot(v, v);
+}
+
+inline float length(dvec3 v) {
+	return sqrtf(lengthSqr(v));
+}
+
+inline dvec3 normalize(dvec3 v) {
+	double len = length(v);
+	if (fequal(len, 0.0f)) return dv3(0.0f); // avoid infinity
+	return v/len;
+}
--- src/math/plane.h
+++ src/math/plane.h
@@ -6,6 +6,10 @@ struct Plane {
 		return dot(normal, p) - d;
 	}
 
+	vec3 project(vec3 p) {
+		return p - distanceTo(p)*normal;
+	}
+
 	// point position in half space { -1: inside, 0: on boundary, 1: outside }
 	int classify(vec3 p, float epsilon = 0.0001f) {
 		float distance = distanceTo(p);
@@ -13,15 +17,40 @@ struct Plane {
 		else if (distance < -epsilon) return -1;
 		return 0;
 	}
-
-	vec3 project(vec3 p) {
-		return p - distanceTo(p)*normal;
-	}
 };
 
 // ccw
 Plane makePlane(vec3 p0, vec3 p1, vec3 p2) {
 	Plane result;
+	result.normal = normalize(cross(p1-p0, p2-p0));
+	result.d = dot(result.normal, p0);
+	return result;
+}
+
+struct Planed {
+	dvec3 normal;
+	double d;
+
+	double distanceTo(dvec3 p) {
+		return dot(normal, p) - d;
+	}
+
+	dvec3 project(dvec3 p) {
+		return p - distanceTo(p)*normal;
+	}
+
+	// point position in half space { -1: inside, 0: on boundary, 1: outside }
+	int classify(dvec3 p, double epsilon = 0.0001) {
+		double distance = distanceTo(p);
+		if (distance > epsilon) return 1;
+		else if (distance < -epsilon) return -1;
+		return 0;
+	}
+};
+
+// ccw
+Planed makePlane(dvec3 p0, dvec3 p1, dvec3 p2) {
+	Planed result;
 	result.normal = normalize(cross(p1-p0, p2-p0));
 	result.d = dot(result.normal, p0);
 	return result;
